b185fcb5ff2f2de6120d0f8380e548625a916233
Scott Obray <sobray11@gmail.com>
2017-10-24 11:26:02 -0600

    create quiz page

--- LiveQuiz/quiz/urls.py
+++ LiveQuiz/quiz/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^join/$', views.student_join, name='join'),
     url(r'^display/$', views.display_join_url, name='display'),
+    url(r'^create/$', views.create, name='create'),
     url(r'^createQuiz/$', views.create_quiz, name='create_quiz'),
 
 
